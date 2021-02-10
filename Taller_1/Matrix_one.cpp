61749b6e0262818b049c05cf75f90155834c2e38
Nico Quintero <juan24092002@gmail.com>
2021-02-10 10:30:30 -0500

    Carpeta del primer taller

---
+++ Taller_1/Matrix_one.cpp
@@ -0,0 +1,18 @@
+class MatrixOne{
+  private:
+    int *array;
+    int dimx;
+    int dimy;
+
+  public:
+    MatrixOne(int m, int n);
+    ~MatrixOne();
+
+    int get_dimx() { return dimx; }
+    int get_dimy() { return dimy; }
+    int get(int x, int y) { return array[y * dimx + x]; }
+    void set(int x, int y, int val) {array[y * dimx + x] = val;}
+
+    MatrixOne& operator=(MatrixOne& M);
+    void display();
+};
---
+++ Taller_1/Matrix_one.hpp
@@ -0,0 +1,21 @@
+#ifndef MATRIX_ONE
+#define MATRIX_ONE
+class MatrixOne{
+  private:
+    int *array;
+    int dimx;
+    int dimy;
+
+  public:
+    MatrixOne(int m, int n);
+    ~MatrixOne();
+
+    int get_dimx() { return dimx; }
+    int get_dimy() { return dimy; }
+    int get(int x, int y) { return array[y * dimx + x]; }
+    void set(int x, int y, int val) {array[y * dimx + x] = val;}
+
+    MatrixOne& operator=(MatrixOne& M);
+    void display();
+};
+#endif
