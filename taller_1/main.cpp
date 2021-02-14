e3595bb730d64d96799e250ed788818103348f04
DaveAlsina <davealsina@gmail.com>
2021-02-13 21:38:34 -0500

    ejecucion serial del producto de matrices, con cambios en el main que presentaba errores

--- taller_1/main.cpp
+++ taller_1/main.cpp
@@ -1,6 +1,16 @@
 #include "matrix_one_serial.hpp"
 #include <iostream>
 
+# include <sys/time.h>
+
+double gettime () {
+	struct timeval tp ;
+	gettimeofday(&tp, NULL) ;
+
+	return tp.tv_sec + tp.tv_usec/(double)1.0e6 ;
+}
+
+
 const int DIMX = 3;
 const int DIMY = 5;
 
@@ -19,20 +29,26 @@ int main ( int argc , char ** argv ) {
 
 	MatrixOne M1(DIMY , size);
 	MatrixOne M2(size , DIMX);
-	M1. display ();
-	M2. display ();
 
+	M1.display ();
+	printf("\n");
+	M2.display ();
+	printf("\n");
+
+	double tstart = gettime ();
+	MatrixOne R = M1*M2;
+	double tstop = gettime ();
+
+	R.display ();
 	printf("\n");
 
 	M2 = M1;
 	M2.display();
-	// double tstart = gettime ();
-	// MatrixOne <int > R = M1*M2;
-	// double tstop = gettime ();
-	//
-	// //R. display ();
-	// printf (" %d\n",R. get (0 ,0));
-	// printf (" Time : %f\n", tstop - tstart );
+	printf("\n");
+
+
+	printf (" %d\n",R. get (0 ,0));
+	printf (" Time : %f\n", tstop - tstart );
 
 	return 0;
 }
