8010fef1b9df9b7f735b8f1dc18b4338b1260567
DaveAlsina <davealsina@gmail.com>
2021-02-13 22:21:52 -0500

    modificación del main_serial para que pueda repetir varias veces las operaciones seriales y calcule así la media y la desviasión estandar

--- taller_1/main_serial.cpp
+++ taller_1/main_serial.cpp
@@ -1,7 +1,8 @@
 #include "matrix_one_serial.hpp"
 #include <iostream>
-
-# include <sys/time.h>
+#include <sys/time.h>
+#include <vector>
+#include <math.h>
 
 double gettime () {
 	struct timeval tp ;
@@ -10,13 +11,40 @@ double gettime () {
 	return tp.tv_sec + tp.tv_usec/(double)1.0e6 ;
 }
 
+double mean(vector<double> data){
+
+	double average = 0;
+
+	for(int i = 0; i < data.size(); i++)
+		average += data[i];
+
+	average /= data.size();
+		
+	return average;
+}
+
+
+double stdDeviation(vector<double> data){
+
+	double variance = 0;
+	double average = mean(data);
+
+	for(int i = 0; i < data.size(); i++)
+		variance += pow( (data[i] - average), 2 );
+		
+	variance /= data.size();
+
+	return sqrt(variance);
+}
+
+
 
 const int DIMX = 3;
 const int DIMY = 5;
 
 int main ( int argc , char ** argv ) {
 
-	if (argc < 2){
+	if (argc < 3){
 
 		printf("error, quantity of entered parameters is less than expected\n");
 		printf("expected 1 and got: ");
@@ -25,30 +53,50 @@ int main ( int argc , char ** argv ) {
 		return 0;	
 	}
 
-	int size = atoi(argv [1]) ;
 
-	MatrixOne M1(DIMY , size);
-	MatrixOne M2(size , DIMX);
+	int size = atoi(argv [1]) ;			//conversión del argumento 1 a size
+	int nreps = atoi(argv [2]);			//conversión del argimento 2 a numero de repeticiones del main
+	vector<double> time(nreps,0);
 
-	M1.display ();
-	printf("\n");
-	M2.display ();
-	printf("\n");
+	for (int reps = 0; reps < nreps; reps++){
 
-	double tstart = gettime ();
-	MatrixOne R = M1*M2;
-	double tstop = gettime ();
+		MatrixOne M1(DIMY , size);
+		MatrixOne M2(size , DIMX);
 
-	R.display ();
-	printf("\n");
+	//	M1.display ();
+	//	printf("\n");
+	//	M2.display ();
+	//	printf("\n");
 
-	M2 = M1;
-	M2.display();
-	printf("\n");
+		double tstart = gettime ();
+		MatrixOne R = M1*M2;
+		double tstop = gettime ();
+
+	//	R.display ();
+	//	printf("\n");
+
+	//	M2 = M1;
+	//	M2.display();
+	//	printf("\n");
 
 
-	printf (" %d\n",R. get (0 ,0));
-	printf (" Time : %f\n", tstop - tstart );
+	//	printf (" %d\n",R. get (0 ,0));
+	//	printf (" Time : %f\n", tstop - tstart );
+
+		time.push_back(tstop - tstart);
+	}
+
+	cout << endl;
+	cout << mean(time) << ", " << stdDeviation(time)<< ", "<< nreps << ", "<< size << endl;
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
