9b5071c24eb3970612c1c586e30fe4fb70290e3c
Nico Quintero <juan24092002@gmail.com>
2021-03-10 13:45:42 -0500

    Basic functions of the DinArray

--- Taller_2/merge_ser.cpp
+++ Taller_2/merge_ser.cpp
@@ -1,7 +1,7 @@
 #include "merge_ser.hpp"
 
 DinArray::DinArray(long size, int type, string name){
-  *p = new long[size];
+  p = new long[size];
   sz = size;
   if(type == 1){
     fill_arr();
@@ -20,31 +20,31 @@ DinArray::~DinArray(){
 }
 
 void DinArray::fill_arr(){
-  minstd_rand0 rng(seed);
+  minstd_rand0 rng(123);
 
-  const long max_num = size;
+  const long max_num = sz;
   //Generation of a sequence of (pseudo)random numbers
   rng();
-  for(long i = 0; i < size; ++i){
+  for(long i = 0; i < sz; ++i){
     long num = long(max_num * ( double(rng()) / rng.max() ));
-    a[i] = num;
+    p[i] = num;
   }
 }
 
 void DinArray::fill_non_rep_arr(){
-  minstd_rand0 rng(seed);
+  minstd_rand0 rng(123);
 
-  const long max_num = 3*size;
+  const long max_num = 3 * sz;
   //use set to profit from the count method
-  set<long> numbers;
+  std::set< long> numbers;
   //Generation of a sequence of (pseudo)random numbers
   rng();
   long count = 0;
-  while(count < size){
+  while(count < sz){
     long num = long(max_num * ( double(rng()) / rng.max() ));
     if(numbers.count(num) == 0){ //if number not in set
       numbers.insert(num);
-      a[count] = num;
+      p[count] = num;
       count++;
     }
   }
@@ -52,23 +52,23 @@ void DinArray::fill_non_rep_arr(){
 
 void DinArray::copy_vector(string name){
   ifstream In(name);
-  for(long i = 0; i < size; ++i)
-    In >> a[i];
+  for(long i = 0; i < sz; ++i)
+    In >> p[i];
   In.close();
 }
 
 void DinArray::write_vector(string name){
   ofstream Out(name);
-  for(long i = 0; i < size; ++i)
-    Out << a[i] << " ";
+  for(long i = 0; i < sz; ++i)
+    Out << p[i] << " ";
   Out << "\n";
   Out.close();
 }
 
 void DinArray::display(){
   printf("[");
-  for(int i = 0; i < size - 1; i++){
+  for(int i = 0; i < sz - 1; i++){
     printf("%d", p[i]);
   }
-  printf("%d]\n", p[size - 1]);
+  printf("%d]\n", p[sz - 1]);
 }
