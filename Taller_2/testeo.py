4f6f5f1e0b8999690a1c38d560a9cd8a6630b75b
DaveAlsina <davealsina@gmail.com>
2021-03-20 02:06:00 -0500

    subido de archivo para fusión de los datos de los csv con pandas

---
+++ Taller_2/testeo.py
@@ -0,0 +1,35 @@
+import re
+import os 
+import pandas as pd
+
+names = os.listdir('./')
+
+def createDataframe():
+
+    results_files = re.compile(r'result.*.csv')
+    numThreads = re.compile(r'\d+')
+    files = []
+
+    for filename in names: 
+        if results_files.search(filename) != None:
+            files.append(filename)
+
+    computingData = pd.read_csv(files[0])
+    nthr = int(numThreads.search(files[0]).group())
+
+    computingData['nthreads'] = [nthr] * len(computingData)
+
+    for f in files[1:]:
+
+        computingDataTmp = pd.read_csv(f)
+        nthr = int(numThreads.search(f).group())
+
+        computingDataTmp['nthreads'] = [nthr] * len(computingDataTmp)
+        computingData = pd.concat([computingData, computingDataTmp])
+
+    computingData.sort_values(by=['nthreads'], inplace = True, ascending = True)
+    print(computingData)
+
+
+
+createDataframe()
