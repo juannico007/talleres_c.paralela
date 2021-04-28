681c588adaa5e042194250de2cb60238cbdcd3ea
juannico007 <juan24092002@gmail.com>
2021-04-28 11:42:21 -0500

    Parallel implementation and test image

--- taller_3/ejemplos_clases/reconstruct_par_v2.cpp
+++ taller_3/ejemplos_clases/reconstruct_par_v2.cpp
@@ -69,12 +69,16 @@ int main(int argc, char** argv){
 	//read image to buff
 	pgmread(filename, buff, m, n);
 
-	for(int i = 0; i < m; i++){
-		for(int j = 0; j < n; j++){
-		  printf(" %.0f", buff[i*n+j]); 
-		}
-		printf("\n");
-	}
+	/* 
+ 	 * print the matrix of the pgm image
+	 */
+
+//	for(int i = 0; i < m; i++){
+//		for(int j = 0; j < n; j++){
+//		  printf(" %.0f", buff[i*n+j]); 
+//		}
+//		printf("\n");
+//	}
 	
 
 	/*
@@ -103,8 +107,8 @@ int main(int argc, char** argv){
 /*
  *	verify scatter of buffer
  */	
-//	printf("Im process number %d of %d and I received from %d to %d, I recieved \n ", rank, size, rank*chunk_size, (rank + 1)*chunk_size);`
-//	
+//	printf("Im process number %d of %d and I received from %d to %d, I recieved \n ", rank, size, rank*chunk_size, (rank + 1)*chunk_size);
+//
 //	for(int i = 0; i < n_rows; i++){
 //		for(int j = 0; j < n; j++){
 //			printf(" %d: %.0f", rank*chunk_size + i*n + j, local_edge[i*n + j]);
@@ -112,24 +116,21 @@ int main(int argc, char** argv){
 //		printf("\n");
 //	}
 //	printf("\n\n");
-//
-//
+
+
 	
 	//Set white halos of old_b and new_b
 	if(rank == 0){
 		for(int i = 0; i < n + 2; i++){			//Set upper edge
 			new_b[i] = 255;
 			old_b[i] = 255;	
-		}
-	}else if(rank == size - 1){					//Set lower edge
-		for(int i = 0; i < n + 2; i++){
+		}	
+	}else if(rank == size - 1){
+		for(int i = 0; i < n + 2; i++){			//Set lower edge
 			new_b[(n+2)*(n_rows+1) + i] = 255;
 			old_b[(n+2)*(n_rows+1) + i] = 255;
 		}
 	}
-
-
-
 
 	for(int i = 0; i < n_rows+1; i++){
 		new_b[i*(n+2)+0] = 255;					//Set left edge
@@ -143,55 +144,101 @@ int main(int argc, char** argv){
 		  old_b[(i+1)*(n+2)+(j+1)] = local_edge[i*n+j]; 
 		}
 	}
+/*
+ *	print for debug
+ *
+ */
 
-	for(int i = 0; i < n_rows+2; i++){
-		for(int j = 0; j < n+2; j++){
-			printf(" %d: %.0f", rank, old_b[i*n + j]);
+//	for(int i = 0; i < n_rows+2; i++){
+//		for(int j = 0; j < n+2; j++){
+//			printf(" %d: %.0f", rank, old_b[i*(n+2) + j]);
+//		}
+//		printf("\n");
+//	}
+//	printf("\n\n");
+//
+//	MPI_Barrier(comm);
+//	printf("**********************************************************************************\n");
+//
+
+	int next_p = rank + 1;
+	int past_p = rank - 1;
+
+	double tstart = gettime();
+
+	//Loop over iterations-------------------------------------
+	
+	for(int it = 0; it < N; it++){
+		//Set communication between processes to share last ando first partition row
+		if(rank == 0){
+			MPI_Send(&old_b[(n+2)*n_rows], n+2, MPI_FLOAT, next_p, 1, comm);
+			MPI_Recv(&old_b[(n+2)*(n_rows+1)], n+2, MPI_FLOAT, next_p, 0, comm, MPI_STATUS_IGNORE);
+		}else if(rank == size - 1){
+			MPI_Send(&old_b[(n+2)*1], n+2, MPI_FLOAT, past_p, 0, comm);	
+			MPI_Recv(&old_b[(n+2)*0], n+2, MPI_FLOAT, past_p, 1, comm, MPI_STATUS_IGNORE);
+		}else{
+			//Send and receive from past process
+			MPI_Send(&old_b[(n+2)*1], n+2, MPI_FLOAT, past_p, 0, comm);	
+			MPI_Recv(&old_b[(n+2)*0], n+2, MPI_FLOAT, past_p, 1, comm, MPI_STATUS_IGNORE);
+			//Send and receive from next process
+			MPI_Send(&old_b[(n+2)*n_rows], n+2, MPI_FLOAT, next_p, 1, comm);
+			MPI_Recv(&old_b[(n+2)*(n_rows+1)], n+2, MPI_FLOAT, next_p, 0, comm, MPI_STATUS_IGNORE);
 		}
-		printf("\n");
+		//get new values (do not update the halo)
+		for(int i = 1; i < n_rows+1; i++){
+			for(int j = 1; j < n+1; j++){
+				new_b[i*(n+2)+j] = (old_b[i*(n+2)+(j-1)]+
+				old_b[i*(n+2)+(j+1)]+
+				old_b[(i-1)*(n+2)+j]+
+				old_b[(i+1)*(n+2)+j]-
+				local_edge[(i-1)*n+(j-1)]) / 4;
+				}
+			}
+
+		//backup the new to the old array (without the halo)
+		for(int i = 1; i < n_rows+1; i++){
+			for(int j = 1; j < n+1; j++){
+				old_b[i*(n+2)+j] = new_b[i*(n+2)+j];
+			}
+		}
 	}
-	printf("\n\n");
+	//---------------------------------------------------------
 
+	double tstop = gettime();
 
-
-//	double tstart = gettime();
-//
-//	//Loop over iterations-------------------------------------
-//	for(int it = 0; it < N; it++){
-//		//get new values (do not update the halo)
-//		for(int i = 1; i < m+1; i++){
-//		  for(int j = 1; j < n+1; j++){
-//			new_b[i*(n+2)+j] = (old_b[i*(n+2)+(j-1)]+
-//						 old_b[i*(n+2)+(j+1)]+
-//						 old_b[(i-1)*(n+2)+j]+
-//						 old_b[(i+1)*(n+2)+j]-
-//						 edge[i*(n+2)+j]) / 4;
-//		  }
-//		}
-//
-//		//backup the new to the old array (without the halo)
-//		for(int i = 1; i < m+1; i++){
-//		  for(int j = 1; j < n+1; j++){
-//			old_b[i*(n+2)+j] = new_b[i*(n+2)+j];
-//		  }
-//		}
-//	}
-//	//---------------------------------------------------------
-//
-//	double tstop = gettime();
-//
+/*
+ *	Print for debug
+ */
 //	printf("\nTime taken was  %f seconds\n", tstop-tstart);
-//
-//	//copy old_b back to buff
-//	for(int i = 1; i < m+1; i++){
-//		for(int j = 1; j < n+1; j++){
-//		  buff[(i-1)*n+(j-1)] = old_b[i*(n+2)+j];
+//	for(int i = 0; i < n_rows+2; i++){
+//		for(int j = 0; j < n+2; j++){
+//			printf(" %d: %.0f", rank, old_b[i*(n+2) + j]);
 //		}
+//		printf("\n");
 //	}
+//	printf("\n\n");
+
+
+	for(int i = 0; i < n_rows; i++){
+		for(int j = 0; j < n; j++){
+		  local_edge[i*n+j] = old_b[(i+1)*(n+2)+(j+1)];  
+		}
+	}
+
+//	for(int i = 0; i < n_rows; i++){
+//		for(int j = 0; j < n; j++){
+//			printf(" %d: %.0f", rank*chunk_size + i*n + j, local_edge[i*n + j]);
+//		}
+//		printf("\n");
+//	}
+//	printf("\n\n");
 //
-//	//write image 
+
+	MPI_Gather(local_edge, chunk_size, MPI_FLOAT, buff, chunk_size, MPI_FLOAT, 0, comm); 
+
+	//write image 
 //	pgmwrite(filename2, buff, m, n);
-//
+
 
 	//free local arrays	
 	delete[] old_b;
@@ -199,6 +246,8 @@ int main(int argc, char** argv){
 	delete[] local_edge;
 	//end parallel region
 	MPI_Finalize();
+
+	pgmwrite(filename2, buff, m, n);	
 
 	//free edge, old_b and new_b
 	//delete[] edge;
