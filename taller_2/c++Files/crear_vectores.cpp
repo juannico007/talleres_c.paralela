6d6a2791b2c0ff112485d1c345116a5a91faaaf7
DaveAlsina <sandavid040@gmail.com>
2021-06-16 17:41:48 -0500

    renombrado de carpeta

--- taller_2/c++Files/crear_vectores.cpp
+++ taller_2/c++Files/crear_vectores.cpp
@@ -4,8 +4,10 @@
 
 int main(int argc, char** argv){
   int n = atoi(argv[1]);
+
   DinArray arr(n, 2);
-  string name = "vector_" + to_string(n) + ".dat";
+  string name = "../data/vector_" + to_string(n) + ".dat";
   arr.write_vector(name);
+
   return 0;
 }
